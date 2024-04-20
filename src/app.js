a8c4246c13e4e9f9ad0c78bdbc5de9745f341d1c
Alyda <lourenaalyda@gmail.com>
2024-04-20 00:25:05 -0300

    Hello word na API, rotas e status code

--- src/app.js
+++ src/app.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const app = express();
 
+//Usamos a função get toda vez que queremos pedir algum dado.
+//função get recebe dois parâmetros:
+
+app.get('/', (req, res) => res.status(200).json({message: 'olá mundo, funcionando!!!'}));
+
+//1° parâmetro '/': é a rota
+//2° parâmetro (req, res) => {}: Este espera uma função de callback
+
+//req: Essa é a Request (ou requisição), é por meio dela que recebemos os dados (envio por query, params e body);
+//res: Essa é a Response (ou resposta), é por meio dela que respondemos o que nos é solicitado;
+
+
+
 module.exports = app;
