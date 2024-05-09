f7b0cb46e07fce2cde288363e8d4d4e59a0e2523
Alyda <lourenaalyda@gmail.com>
2024-05-09 16:30:14 -0300

    listando por meio do get e criando endpoint

--- src/app.js
+++ src/app.js
@@ -14,5 +14,22 @@ app.get('/', (req, res) => res.status(200).json({message: 'olá mundo, funcionan
 //res: Essa é a Response (ou resposta), é por meio dela que respondemos o que nos é solicitado;
 
 
+//Listando times por meio do método GET
+const teams = [
+    {
+        id: 1,
+        name: 'São Paulo Futebol Clube',
+        initials: 'SPF',
+    },
+    {
+        id: 2,
+        name: 'Clube Atlético Mineiro',
+        initials: 'CAM',
+    },
+];
+
+//Agora, você vai criar um endpoint do tipo GET com a rota /teams
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
 
 module.exports = app;
